ce57a116377f80cb8695c857cbc0ef5b54099def
Owen Stevens <a1833201@adelaide.edu.au>
2025-06-20 15:34:08 +0000

    [automatic commit] 2025-06-20 15:34:08 UTC 119.15.78.14

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -18,7 +18,7 @@ router.post('/register', async (req, res) => {
 
   try {
     const [result] = await db.query(
-      INSERT INTO Users (username, email, password_hash, role) VALUES
+      INSERT INTO Users (username, email, password_hash, role) ,VALUES
 
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 
