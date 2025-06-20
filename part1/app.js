98309abc2246bc3fda3cb8658d19c310ab8063ad
Owen Stevens <a1833201@adelaide.edu.au>
2025-06-20 10:47:11 +0000

    [automatic commit] 2025-06-20 10:47:11 UTC 119.15.78.14

--- part1/app.js
+++ part1/app.js
@@ -24,14 +24,14 @@ let db;
     });
 
     // Create a table if it doesn't exist
-    await db.execute('
+    await db.execute(`
         CREATE TABLE IF NOT EXISTS Users (
       user_id INT AUTO_INCREMENT PRIMARY KEY,
       username VARCHAR(50) NOT NULL UNIQUE,
       email VARCHAR(100) NOT NULL,
       password_hash VARCHAR(255) NOT NULL,
       role ENUM('owner', 'walker') NOT NULL
-    );')
+    `);')
 
     // Insert data if table is empty
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
