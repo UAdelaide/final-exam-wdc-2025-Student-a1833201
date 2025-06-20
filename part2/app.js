d5640ad66822ff512583109c9bb6c6f380a43462
Owen Stevens <a1833201@adelaide.edu.au>
2025-06-20 16:16:46 +0000

    [automatic commit] 2025-06-20 16:16:46 UTC 119.15.78.14

--- part2/app.js
+++ part2/app.js
@@ -17,13 +17,23 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-app.post('/logout', (req, res) => {
-req.session.destroy(!err => {
-    if (err){
+app.post('/logout', function(req, res) {
+  function destroySession(callback) {
+    req.session.destroy(function(err) {
+      callback(err); // Call the callback with the error (if any)
+    });
+  }
+
+  destroySession(function(err) {
+    if (err) {
+      console.error('Error destroying session:', err); // Log the error for debugging
+      return res.status(500).json({ error: 'Could not log out' });
+    }
+
     res.clearCookie('connect.sid'); // Clear the session cookie
     res.json({ message: 'Logout successful' });
-    }
   });
 });
+
 // Export the app instead of listening here
 module.exports = app;
