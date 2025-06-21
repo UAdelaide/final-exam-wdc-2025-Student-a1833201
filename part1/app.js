acaaeb4bc31c2c0cad9a01609b9a4b82cac40307
Owen Stevens <a1833201@adelaide.edu.au>
2025-06-21 03:22:56 +0000

    [automatic commit] 2025-06-21 03:22:56 UTC ;; communications error to 208.67.222.222#53: timed out 119.15.78.14

--- part1/app.js
+++ part1/app.js
@@ -21,7 +21,8 @@ let db;
       password:'dog123'
     });
     await connection.query('DROP DATABSE IF EXISTS DogWalkService');
-    await connection.query('CREATE DATA')
+    await connection.query('CREATE DATABASE DogWalkService');
+    await connection
   }
 }
 
