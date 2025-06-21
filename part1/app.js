c6691f5e3654b606f1dd25583e799d88f6e994be
Owen Stevens <a1833201@adelaide.edu.au>
2025-06-21 03:33:53 +0000

    [automatic commit] 2025-06-21 03:33:52 UTC 119.15.78.14

--- part1/app.js
+++ part1/app.js
@@ -44,7 +44,7 @@ async function insertInitialData() {
   if (users[0][0].count === 0) {
     await db.execute(`
       INSERT INTO Users (username, email, password_hash, role) VALUES
-      ('alice123', 'alice@example.com', 'hashed_password_1', 'owner'),
+      ('alice123', 'alice@example.com', 'hashed_password123', 'owner'),
       ('bobwalker', 'bob@example.com', 'hashed_password_2', 'walker'),
       ('carol123', 'carol@example.com', 'hashed_password_3', 'owner')
     `);
